5a68679454081e2baab201325aaafb229c3eddd4
Morfiadakis Emmanouil <manosmorf97@gmail.com>
2023-08-10 14:00:52 +0300

    Update

--- days_off/days_off/urls.py
+++ days_off/days_off/urls.py
@@ -19,6 +19,6 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('user_authentication.urls')),
-    path('/accounts',include('django.contrib.auth.urls'))
+    path('home/',include('user_authentication.urls')),
+    path('accounts/',include('django.contrib.auth.urls'))
 ]
