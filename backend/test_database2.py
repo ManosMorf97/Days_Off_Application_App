5e8d7ae4b76d2759fb4a0c51dfe80525b92a0e25
Morfiadakis Emmanouil <manosmorf97@gmail.com>
2023-09-04 14:55:13 +0300

    Try

---
+++ backend/test_database2.py
@@ -0,0 +1,28 @@
+from connection import *
+
+def delete1():
+    db,cursor=connect()
+    cursor.execute("DROP TABLE IF EXISTS Request")
+    db.commit()
+    cursor.execute("DROP TABLE IF EXISTS Decision")
+    db.commit()
+    cursor.execute("DROP TABLE IF EXISTS Employee")
+    db.commit()
+    disconnect(db,cursor)
+
+def statement():
+    delete1()
+    db,cursor=connect()
+    cursor.execute("create table Employee(email varchar(30) not null,Name varchar(20) not null,Surname varchar(20) not null,NormalDaysOff int not null,ParentialDaysOff int not null,DiseaseDaysOff int not null,primary key(email))ENGINE=InnoDB;")
+    db.commit()
+    cursor.execute("insert into  Employee values ('johnd97@gmail.com','John','Doe',25,50,90)")
+    db.commit()
+    disconnect(db,cursor)
+
+#statement()
+db,cursor=connect()
+cursor.execute("select * from Employee")
+print(cursor.fetchall())
+db.commit()
+disconnect(db,cursor)
+
