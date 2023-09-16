7915b9c5d0d9e0f7c2da8e7a5a79522ba360fb8e
Morfiadakis Emmanouil <manosmorf97@gmail.com>
2023-09-16 18:57:06 +0300

    update

--- Request_Days_Off_Application_BACKUP/backend/secretP.py
+++
@@ -1,2 +0,0 @@
-mysql_pwd='x#9kQL4X'
-broker_pwd='mh2UOkJsEc3raqw'
--- Request_Days_Off_Application_BACKUP/json_testing.py
+++
@@ -1,7 +0,0 @@
-import json
-
-dict1={'name':'Manos','parents':['MILTOS','SOULA']}
-json_d=json.dumps(dict1)
-print(json_d)
-json_l=json.loads(json_d)
-print(json_l)
