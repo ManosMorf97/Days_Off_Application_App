c754e4b3a787e8de198fa2f4328cf683690ef35d
Morfiadakis Emmanouil <manosmorf97@gmail.com>
2023-09-16 15:16:34 +0300

    p

--- days_off/days_off/settings.py
+++ days_off/days_off/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-eo5&g@9*12@qstyk+t($%^oc(2(857*i20tx$m8!ih0mjjj_^w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
